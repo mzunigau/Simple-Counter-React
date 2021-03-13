d6b8dd0cf6917cc4d1b28549343fa51d767a9cd3
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-03-13 03:32:33 +0000

    contador con estilos

--- src/js/component/secondscounter.js
+++ src/js/component/secondscounter.js
@@ -5,13 +5,25 @@ import PropTypes from "prop-types";
 
 export function SecondsCounter(props) {
 	return (
-		<div className="container">
-			<span className="badge badge-secondary">{props.digSix % 10}</span>
-			<span className="badge badge-secondary">{props.digFive % 10}</span>
-			<span className="badge badge-secondary">{props.digFour % 10}</span>
-			<span className="badge badge-secondary">{props.digThree % 10}</span>
-			<span className="badge badge-secondary">{props.digTwo % 10}</span>
-			<span className="badge badge-secondary">{props.digOne % 10}</span>
+		<div className="bigCounter">
+			<div id="clock">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="60"
+					height="60"
+					fill="currentColor"
+					className="bi bi-clock"
+					viewBox="0 0 16 16">
+					<path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+					<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+				</svg>
+			</div>
+			<div>{props.digSix % 10}</div>
+			<div>{props.digFive % 10}</div>
+			<div>{props.digFour % 10}</div>
+			<div>{props.digThree % 10}</div>
+			<div>{props.digTwo % 10}</div>
+			<div>{props.digOne % 10}</div>
 		</div>
 	);
 }
